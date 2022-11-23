075abbcde54bd792c906eba3d01853f720343326
Cameron Fung <Cameronfuung@gmail.com>
2022-11-23 11:41:29 -0800

    added reading and writing to firestore

--- scripts/main.js
+++ scripts/main.js
@@ -10,8 +10,8 @@ if (localStorage.login === "true") {
         const template = document.querySelector("#playlist-template");
         console.table(doc)
         // console.log(doc[0].images[0].imageUrl)
-
-        for (let i = 0 ; i < 3; i++){
+        document.querySelector("#playlists-holder").innerHTML = "";
+        for (let i = 0 ; i < doc.length; i++){
             let imageUrl = doc[i].imageUrl;
             let name = doc[i].name;
             let trackUrl = doc[i].trackUrl;
@@ -23,10 +23,11 @@ if (localStorage.login === "true") {
             let data = {
                 name : name,
                 image: imageUrl,
-                songs : []
+                songs : [],
+                trackUrl : trackUrl
             }
-            writePlaylists(name, data);
-            newEle.querySelector(".playlist").onclick = () => getSongs(name); 
+            // writePlaylists(name, data);
+            newEle.querySelector(".playlist").onclick = () => getSongs(trackUrl, imageUrl, name); 
             document.querySelector("#playlists-holder").appendChild(newEle);
         }
     }
@@ -35,19 +36,43 @@ if (localStorage.login === "true") {
     xhr.send();
 }
 
-const getSongs = (name) =>{
-    // const xhr = new XMLHttpRequest();
-    // // console.log("xhr", xhr);
-    // xhr.onload = function() {
-    //     doc = JSON.parse(this.responseText);
-
-    // }
-
-    // xhr.open("GET", "http://localhost:8000/getTracks");
-    // xhr.send();
-
-    console.log(name)
+const getSongs = (url, img, name) =>{
+    localStorage.setItem("name", name)
+    localStorage.setItem("song", url)
+    localStorage.setItem("img", img)
+    window.location.replace("http://localhost:8000/playlist-view")
 }
+
+ready(() => {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid)
+            currentUser.collection("playlists")
+            .get()
+            .then(allPlaylists => {
+                allPlaylists.forEach(element => {
+                    const template = document.querySelector("#playlist-template");
+    
+                    let name = element.data().name;
+                    let imageUrl = element.data().image;
+                    let trackUrl = element.data().trackUrl;  
+    
+                    localStorage.setItem(name, trackUrl);
+    
+                    let newEle = template.content.cloneNode(true);
+                    newEle.querySelector(".playlist-name").textContent = name;
+                    newEle.querySelector(".playlist-image").src = imageUrl;
+
+                    newEle.querySelector(".playlist").onclick = () => getSongs(trackUrl, imageUrl, name); 
+                    document.querySelector("#playlists-holder").appendChild(newEle);
+                });
+            })
+        } else {
+    
+        }
+    });
+});
 
 /**
  * writing the playlist info to the db
@@ -62,4 +87,15 @@ const writePlaylists = (name, data) => {
 
         }
     });
+}
+
+// callback function declaration
+function ready(callback) {
+    if (document.readyState != "loading") {
+        callback();
+        console.log("ready state is 'complete'");
+    } else {
+        document.addEventListener("DOMContentLoaded", callback);
+        console.log("Listener was invoked");
+    }
 }
