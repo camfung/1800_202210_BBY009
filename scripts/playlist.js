9aecb9656d71f128e23345d155f367916b6273af
Cameron Fung <Cameronfuung@gmail.com>
2022-11-28 21:46:23 -0800

    added progress bar ui

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -5,6 +5,7 @@ const get_tracks = () => {
     console.log("get tracks called")
     let href = localStorage.getItem("song");
     const xhr = new XMLHttpRequest();
+    progress();
     xhr.onload = function() {
         // doc = JSON.parse(this.responseText);
         let doc = this.responseText;
@@ -15,4 +16,21 @@ const get_tracks = () => {
     
     xhr.open("GET", "http://localhost:8000/playlist-tracks" + "?href="+href);
     xhr.send();   
+}
+
+const progress = () => {
+    let i = 5;
+if (i == 5) {
+    i = 6;
+    let percentage = document.getElementById("percentage");
+    let progress;
+    let elem = document.getElementById("myBar");
+    let width = 5;
+    let id = setInterval(frame, 1000);
+    function frame() {
+        width++;
+        percentage.textContent = width + "%"
+        elem.style.width = width + "%";
+    }
+  }
 }
